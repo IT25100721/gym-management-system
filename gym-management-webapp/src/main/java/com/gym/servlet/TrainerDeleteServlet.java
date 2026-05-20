5f40367470aad030324cfb29870b179fe94ac832
IT25100721 <it25100721@my.sliit.lk>
2026-05-20 23:00:10 +0530

    Update TrainerDeleteServlet.java

--- gym-management-webapp/src/main/java/com/gym/servlet/TrainerDeleteServlet.java
+++ gym-management-webapp/src/main/java/com/gym/servlet/TrainerDeleteServlet.java
@@ -4,7 +4,7 @@ import com.gym.dao.TrainerDAO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-
+//Trainer Delete
 @WebServlet("/trainer-delete")
 public class TrainerDeleteServlet extends HttpServlet {
     private TrainerDAO dao = new TrainerDAO();

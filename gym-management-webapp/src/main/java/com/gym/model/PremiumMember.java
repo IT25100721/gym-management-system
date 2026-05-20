988f834395dac73dae8c39a8ddd90c76057edbf4
IT25100721 <it25100721@my.sliit.lk>
2026-05-20 22:03:07 +0530

    Update premiumMember

--- gym-management-webapp/src/main/java/com/gym/model/PremiumMember.java
+++ gym-management-webapp/src/main/java/com/gym/model/PremiumMember.java
@@ -1,7 +1,7 @@
 package com.gym.model;
 
 public class PremiumMember extends Member {
-    private String premiumBenefits = "Personal Trainer + Sauna + Protein Bar";
+    private String premiumBenefits = "Personal Trainer + Sauna + Protein Bar"; //Output premiumBenefits
 
     public String getPremiumBenefits() { return premiumBenefits; }
     public void setPremiumBenefits(String premiumBenefits) { this.premiumBenefits = premiumBenefits; }

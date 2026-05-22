fe29a1d340a8f151e95bc468fce5519d3b29fb99
IT25101108 <it25101108@my.sliit.lk>
2026-05-23 01:14:57 +0530

    Revert "Revert "Complete Component 06: Workout Plan Management System""

---
+++ src/main/java/com/fitcore/workout/model/ExerciseProgram.java
@@ -0,0 +1,16 @@
+package com.fitcore.workout.model;
+
+import java.io.Serializable;
+
+public abstract class ExerciseProgram implements Serializable {
+    private String programName;
+
+    public ExerciseProgram(String programName) {
+        this.programName = programName;
+    }
+
+    public String getProgramName() { return programName; }
+    public void setProgramName(String programName) { this.programName = programName; }
+
+    public abstract double calculateCaloriesBurned();
+}
---
+++ src/main/java/com/fitcore/workout/model/WorkoutPlan.java
@@ -0,0 +1,28 @@
+package com.fitcore.workout.model;
+
+public class WorkoutPlan extends ExerciseProgram {
+    private String planId;
+    private int durationMinutes;
+    private String fitnessGoal;
+
+    public WorkoutPlan(String planId, String programName, int durationMinutes, String fitnessGoal) {
+        super(programName);
+        this.planId = planId;
+        this.durationMinutes = durationMinutes;
+        this.fitnessGoal = fitnessGoal;
+    }
+
+    public String getPlanId() { return planId; }
+    public void setPlanId(String planId) { this.planId = planId; }
+
+    public int getDurationMinutes() { return durationMinutes; }
+    public void setDurationMinutes(int durationMinutes) { this.durationMinutes = durationMinutes; }
+
+    public String getFitnessGoal() { return fitnessGoal; }
+    public void setFitnessGoal(String fitnessGoal) { this.fitnessGoal = fitnessGoal; }
+
+    @Override
+    public double calculateCaloriesBurned() {
+        return durationMinutes * 5.0;
+    }
+}
